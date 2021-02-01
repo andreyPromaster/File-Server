7f090d9f4f78a2adeb32610d87e48f05b2968fc4
Andrey Yakshuk <and45.rew@ya.ru>
2021-02-01 23:45:00 +0300

    create models for manager

--- fileManager/models.py
+++ fileManager/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class UserContainer(models.Model):
+    email = models.EmailField(max_length=100, unique=True)
+    container = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+
+class Content(models.Model):
+    name_of_blob = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    unique_link_to_blob = models.UUIDField(default=uuid.uuid4, unique=True)
