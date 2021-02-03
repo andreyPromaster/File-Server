9d67c5506ea40006cfea8a0e419f9e2e26216ba4
Andrey Yakshuk <and45.rew@ya.ru>
2021-02-03 18:55:21 +0300

    create azure manager

--- fileManager/azure_storage_services.py
+++ fileManager/azure_storage_services.py
@@ -2,46 +2,32 @@ import os
 import uuid
 
 from azure.storage.blob import BlobServiceClient, BlobClient
-
 from FileServer import settings
 
-MY_BLOB_CONTAINER = "fileChanger"
 
-# Create the BlobServiceClient object which will be used to create a container client
-blob_service_client = BlobServiceClient.from_connection_string(settings.MY_CONNECTION_STRING)
+class AzureStorageManager:
+    def __init__(self):
+        self.blob_service_client = BlobServiceClient.from_connection_string(settings.CONNECTION_STRING_TO_AZURE_STORAGE)
 
-# Create a unique name for the container
-container_name = str(uuid.uuid4())
+    def create_container(self):
+        self.container_name = self.__create_unique_string()
+        self.blob_service_client.create_container(self.container_name)
 
-# Create the container
-container_client = blob_service_client.create_container(container_name)
+    def __create_unique_string(self):
+        return str(uuid.uuid4())
 
-# Create a local directory to hold blob data
-local_path = "./data"
-#os.mkdir(local_path)
+    def upload_file_to_container(self, file, file_name):
+        blob_client = self.blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
 
-# Create a file in the local data directory to upload and download
-local_file_name = str(uuid.uuid4()) + ".txt"
-upload_file_path = os.path.join(local_path, local_file_name)
+        # Upload the created file
+        for chunk in file.chunks():
+            blob_client.upload_blob(chunk)
 
-# Write text to the file
-file = open(upload_file_path, 'w')
-file.write("Hello, World!")
-file.close()
 
-# Create a blob client using the local file name as the name for the blob
-blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-
-print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
-
-# Upload the created file
-with open(upload_file_path, "rb") as data:
-    blob_client.upload_blob(data)
-
-# Download the blob to a local file
-# Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
-download_file_path = os.path.join(local_path, str.replace(local_file_name, '.txt', 'DOWNLOAD.txt'))
-print("\nDownloading blob to \n\t" + download_file_path)
-
-with open(download_file_path, "wb") as download_file:
-    download_file.write(blob_client.download_blob().readall())
+# # Download the blob to a local file
+# # Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
+# download_file_path = os.path.join(local_path, str.replace(local_file_name, '.txt', 'DOWNLOAD.txt'))
+# print("\nDownloading blob to \n\t" + download_file_path)
+#
+# with open(download_file_path, "wb") as download_file:
+#     download_file.write(blob_client.download_blob().readall())
